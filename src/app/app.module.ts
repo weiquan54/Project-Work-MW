f906da677d374c971e26eb08db5e8f4ea89429a4
gui <gui@movingwalls.com>
2023-11-08 15:33:35 +0800

    change main page to calculator 4.0

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -44,7 +44,7 @@ const routes: Routes = [
     FormsModule,
     ReactiveFormsModule,
     HttpClientModule,
-    RouterModule.forRoot(routes),
+    RouterModule.forRoot(routes, { useHash: false }),
     AppRoutingModule,
   ],
   providers: [
